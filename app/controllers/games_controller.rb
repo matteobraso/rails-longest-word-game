e66b2a771ef5c2ec3a9ec8b5ba47ac0dede3b2d8
Matteo Bratto <m.bratto@hotmail.it>
2021-08-28 17:16:22 +0200

    created longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = (params[:word] || "" ).upcase
+    @letters = params[:letters].split('')
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
